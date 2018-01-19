323feb8b3c2b674d1c41db7571ca3b0dca6c3661
Joshua Rose <jrgiant_git@outlook.com>
2018-01-19 16:22:36 -0500

    php initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+unset($CFG);
+global $CFG;
+
+$CFG = new stdClass();
+$CFG->dbType = "mysql";
+$CFG->dbName = "jobapp";
+$CFG->dbUser = "jobapp";
+$CFG->dbPass = "*K3T4&uRUupWv5&F";
+$CFG->dbHost = "jrgiantdevcom.ipagemysql.com";
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Job Application System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    
+</head>
+<body>
+<?php
+require_once("db.php");
+$db = new DB();
+$db->testLink();
+?>    
+</body>
+</html>
